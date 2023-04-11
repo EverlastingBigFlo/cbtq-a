64bd09cc440e4e0e71f54aa2cf4994eed58d9771
EverlastingLilflo <anjorin199@gmail.com>
2023-04-11 21:14:58 +0100

    changes in files

--- style.js
+++ style.js
@@ -1,20 +1,60 @@
-const quizForm = document.getElementById('quiz-form');
-const resultDiv = document.getElementById('result');
-const correctAnswers = ['c','b','c']
 
-quizForm.addEventListener('submit', e=>{
-    e.preventDefault();
 
-    let score =   0;
-    const userAnswers = [quizForm.q1.value,quizForm.q2.value,quizForm.q3.value];
-    userAnswers.forEach((answer,index) =>{
-        if (answer === correctAnswers[index]){
-            score += 1;
-        }
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const quizForm = document.getElementById('quiz-form');
+// const resultDiv = document.getElementById('result');
+// const correctAnswers = ['c','b','c']
+
+// quizForm.addEventListener('submit', e=>{
+//     e.preventDefault();
+
+//     let score =   0;
+//     const userAnswers = [quizForm.q1.value,quizForm.q2.value,quizForm.q3.value];
+//     userAnswers.forEach((answer,index) =>{
+//         if (answer === correctAnswers[index]){
+//             score += 1;
+//         }
+//     });
     
-    // resultDiv.innerHTML = `your score is ${score}/${correctAnswers.length}`;
-})
-function alert(){
-    alert(`your score is ${score}/${correctAnswers.length}`);
-}
+//     // resultDiv.innerHTML = `your score is ${score}/${correctAnswers.length}`;
+// })
+// function alert(){
+//     alert(`your score is ${score}/${correctAnswers.length}`);
+// }
